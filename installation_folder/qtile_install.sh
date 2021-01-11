efe0f0a2e3840e9078dc4691678a41c864c63f13
STR4NG3R <locochon9268@gmail.com>
2021-01-11 01:52:08 -0600

    Update qtile_install.sh
    Fix args

--- installation_folder/qtile_install.sh
+++ installation_folder/qtile_install.sh
@@ -1,4 +1,6 @@
 function install_qtile {
+    PATH_DIR=$1
+    USER_HOME=$2
     yay -S --noconfirm python-psutil qtile network-manager-applet
     cp -R "$PATH_DIR/installation_folder/qtile/" "$USER_HOME/.config/"
 }
