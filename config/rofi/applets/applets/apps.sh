18e600f5b61292fb0e6138caf4faf0ce9afb4334
STR4NG3R <locochon9268@gmail.com>
2021-07-09 11:30:12 -0500

    Added rofi conf

---
+++ config/rofi/applets/applets/apps.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env bash
+
+## Author  : Aditya Shakya
+## Mail    : adi1090x@gmail.com
+## Github  : @adi1090x
+## Twitter : @adi1090x
+
+style="$($HOME/.config/rofi/applets/applets/style.sh)"
+
+dir="$HOME/.config/rofi/applets/applets/configs/$style"
+rofi_command="rofi -theme $dir/apps.rasi"
+
+# Links
+terminal=""
+files=""
+editor=""
+browser=""
+music=""
+settings=""
+
+# Error msg
+msg() {
+	rofi -theme "$HOME/.config/rofi/applets/styles/message.rasi" -e "$1"
+}
+
+# Variable passed to rofi
+options="$terminal\n$files\n$editor\n$browser\n$music\n$settings"
+
+chosen="$(echo -e "$options" | $rofi_command -p "Most Used" -dmenu -selected-row 0)"
+case $chosen in
+    $terminal)
+		if [[ -f /usr/bin/alacritty ]]; then
+			alacritty &
+		elif [[ -f /usr/bin/xfce4-terminal ]]; then
+			xfce4-terminal &
+		else
+			msg "No suitable terminal found!"
+		fi
+        ;;
+    $files)
+		if [[ -f /usr/bin/thunar ]]; then
+			thunar &
+		elif [[ -f /usr/bin/pcmanfm ]]; then
+			pcmanfm &
+		else
+			msg "No suitable file manager found!"
+		fi
+        ;;
+    $editor)
+		if [[ -f /usr/bin/geany ]]; then
+			geany &
+		elif [[ -f /usr/bin/nvim ]]; then
+			nvim &
+		elif [[ -f /usr/bin/mousepad ]]; then
+			mousepad &
+		elif [[ -f /usr/bin/code ]]; then
+			code &
+		else
+			msg "No suitable text editor found!"
+		fi
+        ;;
+    $browser)
+		if [[ -f /usr/bin/firefox ]]; then
+			firefox &
+		elif [[ -f /usr/bin/chromium ]]; then
+			chromium &
+		elif [[ -f /usr/bin/midori ]]; then
+			midori &
+		else
+			msg "No suitable web browser found!"
+		fi
+        ;;
+    $music)
+		if [[ -f /usr/bin/cmus ]]; then
+			alacritty -e cmus &
+		else
+			msg "No suitable music player found!"
+		fi
+        ;;
+    $settings)
+		if [[ -f /usr/bin/xfce4-settings-manager ]]; then
+			xfce4-settings-manager &
+		else
+			msg "No suitable settings manager found!"
+		fi
+        ;;
+esac
