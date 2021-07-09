27e7356ee7f0af0e8574d8b1884c182fde0684d1
STR4NG3R <locochon9268@gmail.com>
2021-07-09 11:41:32 -0500

    Script to download a song from clipboard

---
+++ config/scripts/youtube_download.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+regex='(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
+link=$(xclip -o)
+if [[ $link =~ $regex ]]
+then 
+    notify-send "Starting Download ${link}"
+    cwd=$(pwd)
+    cd $HOME/Music/
+    youtube-dl -x --embed-thumbnail --audio-format mp3 $link
+    notify-send "Downloaded"
+    notify-send 
+    cd $cwd
+  else 
+    notify-send "Invalid Link"
+fi
