2ed85174ae7d4ec6b7b9da823b7b05fbe9c67347
Pablo Martinez <pablo.martinez@eossoftware.mx>
2021-09-04 23:19:28 -0500

    Update auto start

--- config/scripts/autostart.sh
+++ config/scripts/autostart.sh
@@ -9,9 +9,6 @@ function run {
 
 #starting utility applications at boot time
 run numlockx on &
-# xfce4-power-manager &
-# nm-applet &
-# blueberry-tray &
 setxkbmap -layout latam &
 redshift &
 picom &
@@ -20,11 +17,4 @@ picom &
 exec --no-startup-id /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
 nitrogen --restore &
 xfce4-power-manager-settings &
-nm-applet
-
-if [ "$1" == "qtile" ]; then
-    run nm-applet & xfce4-power-manager-settings
-elif [ "$1" == "i3" ]; then 
-  run nm-applet
-fi
-
+udiskie
