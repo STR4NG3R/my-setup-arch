6e1b9aaf90d7b28612d6d9d8580a138acb8d682b
Pablo Martinez <pablo.martinez@eossoftware.mx>
2021-09-04 23:18:00 -0500

    Scripts update

--- installation_folder/i3_install.sh
+++ installation_folder/i3_install.sh
@@ -1,7 +1,7 @@
 function install_i3 {
     PATH_DIR=$1
     USER_HOME=$2
-    trizen -S --noedit --noconfirm i3wm i3-lock i3-gaps-rounded-git pulseaudio-control
+    trizen -S --noedit --noconfirm i3-wm i3-lock pulseaudio-control
     cp -R "$PATH_DIR/installation_folder/i3/*" "$USER_HOME/.config/"
 }
 
