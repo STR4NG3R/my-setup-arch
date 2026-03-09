ef746588f9c2cdbb2e29765ecb1bc79f695220fd
Pablo Eduardo Martinez <pablo980629@hotmail.com>
2026-03-08 20:08:09 -0600

    fix path

--- installation_folder/hyper_install.sh
+++ installation_folder/hyper_install.sh
@@ -2,5 +2,5 @@ function install_hyper {
     PATH_DIR=$1
     USER_HOME=$2
     paru -S wofi hyprland waybar
-    cp -R "$PATH_DIR/installation_folder/i3/*" "$USER_HOME/.config/"
+    cp -R "$PATH_DIR/installation_folder/hyper/*" "$USER_HOME/.config/"
 }
